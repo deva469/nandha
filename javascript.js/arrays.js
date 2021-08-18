732bd907bda03b1a965376ff4dc4b33355a02b36
deva469 <devanandareddy987@gmail.com>
2021-08-18 10:24:22 +0530

    sample

---
+++ javascript.js/arrays.js
@@ -0,0 +1,7 @@
+let b=[7,'java']
+let a =[2 ,4.55,'colour',[23,'jV']]
+
+console.log(a[3])
+for(arr of a){
+console.log(arr)
+}	
---
+++ javascript.js/assending.js
@@ -0,0 +1,15 @@
+var a=3
+var b=5
+var c=2
+if (a>b&&a>c){
+	
+	a>b?console.log("a>b>c"):		console.log("a>c>b")
+}
+if (b>a&&b>c){
+	
+	b>a?console.log("b>a>c"):		console.log("b>c>a")
+}
+if (c>b&&c>a){
+	
+	c>b?console.log("c>b>a"):		console.log("c>a>b")
+}
